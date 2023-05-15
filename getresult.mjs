d5be639801cff09384b0479d1c5d33c5d7353106
Ishan Verma <83359975+Ishan-creed@users.noreply.github.com>
2023-05-16 00:03:25 +0530

    Add files via upload

---
+++ getresult.mjs
@@ -0,0 +1,518 @@
+var data = [ {
+    "Day": "MON",
+    "09:00 AM - 09:55 AM": [
+      "L-18B11EC411  EC41  (SHR) TR7",
+      "P-18B17CI473  IT42  (VKB) CL11",
+      "P-18B17CI474  CS41  (SLD) CL3_1",
+      "P-18B17CI473  CS48  (NVN) CL8",
+      "P-18B17CI473  CS44  (PDG) CL7",
+      "P-18B17CI471  CS45  (HRI) CL4",
+      "T-18B11BT413  BT41  (RJK) TR5",
+      "P-18B17CI471  CS42  (DGA) CL3_2",
+      "P-18B17CI473  CS46  (ALK) CL6",
+      "P-18B17CI471  CS47  (MBI) CL5_1"
+    ],
+    "10:00 AM - 10:55 AM": [
+      "L-18B11MA413  EC41,EM41  (PKP) TR6",
+      "P-18B17CI473  CS46  (ALK) CL6",
+      "P-18B17CI471  CS47  (MBI) CL5_1",
+      "P-18B17CI471  CS42  (DGA) CL3_2",
+      "L-18B11CE414  CE41  (NJP) CR13",
+      "P-18B17CI471  CS45  (HRI) CL4",
+      "P-18B17CI473  CS44  (PDG) CL7",
+      "P-18B17CI473  CS48  (NVN) CL8",
+      "L-18B11MA411  BI41  (SST) TR3",
+      "P-18B17CI474  CS41  (SLD) CL3_1",
+      "P-18B17CI473  IT42  (VKB) CL11",
+      "L-18B11BT412  BT41  (JTV) TR5"
+    ],
+    "11:00 AM - 11:55 AM": [
+      "L-18B11CI411  CS410,IT41,IT42 (PTK) CR6",
+      "L-18B11EC311 EC41 (SRU) TR6",
+      "L-20B11EM411 EM41  (SWT) TR5",
+      "L-18B11BI413  BI41  (RJK) TR3",
+      "P-18B17CI473  CS43  (ALK) CL9_2",
+      "P-18B17CI473  CS42  (PDG) CL8",
+      "L-18B11CI413  CS44,CS45,CS46  (NFK) CR2",
+      "L-18B11CI411  CS47,CS48,CS49  (MBI) CR9"
+    ],
+    "12:00 PM - 12:55PM": [
+      "L-18B11HS411  EC41,EM41,CE41,BT41,BI41,BACK-CE61 (ASA) LT3",
+      "P-18B17CI473  CS43  (ALK) CL9_2",
+      "P-18B17CI473  CS42  (PDG) CL8"
+    ],
+    "01:00 PM - 01:55 PM": [
+      "L-18B11HS411  CS44-CS46, CS65-BACK (TGM) LT3",
+      "P-18B17CI472  CS48  (SGL) CL5_2",
+      "P-18B17CI472  CS49  (AMN) CL6",
+      "P-18B17CI473  CS47  (NVN) CL7",
+      "P-18B17CI473  CS410  (VKB) CL8"
+    ],
+    "02:00 PM - 02:55 PM": [
+      "L-18B11CI413  CS41,CS42,CS43   (NFK) CR1",
+      "L-18B11CI412  CS44,CS45,CS46  (RKI) CR9",
+      "L-19B11CI411  IT41-IT42  (PKR) CR11",
+      "L-18B11CE411  CE41  (AKG) CR14",
+      "T-20B11EM411 EM41  (SWT) TR7",
+      "P-18B17CI472  CS48  (SGL) CL5_2",
+      "P-18B17CI472  CS49  (AMN) CL6",
+      "P-18B17CI473  CS47  (NVN) CL7",
+      "P-18B17CI473  CS410  (VKB) CL8",
+      "L-18B11BI414  BI41,BIM  (SML) TR3",
+      "P-18B17EC473  EC41  (ALK) ECL5",
+      "L-18B11BT413  BT41  (RJK) TR5"
+    ],
+    "03:00 PM - 03:55 PM": [
+      "L-18B11HS411 CS410, IT41,IT42  (TGM) CR8",
+      "P-20B17EM471 EM41  (SWT) ECL7",
+      "L-18B11CE412  CE41  (ASR) CR14",
+      "P-18B17CI471  CS48  (MBI) CL5_2",
+      "L-18B11CI414  CS41,CS42,CS43   (PKP) LT1",
+      "P-18B17CI473  CS45  (PDG) CL9_1",
+      "P-18B17CI474  CS49  (NFK) CL8_1",
+      "P-18B17BI473  BI41  (RJK) BPL",
+      "P-18B17CI474  CS46  (NTJ) CL9_2",
+      "P-18B17EC473  EC41  (ALK) ECL5",
+      "P-18B17CI472  CS44 (RKI) CL1",
+      "L-18B11BT414  BT41  (AKN) TR6"
+    ],
+    "04:00PM - 04:55 PM": [
+      "P-20B17EM471 EM41  (SWT) ECL7",
+      "P-18B17CI471  CS48  (MBI) CL5_2",
+      "L-18B11CI413  CS410,IT41,IT42  (SLD) CR2",
+      "P-18B17CI473  CS45  (PDG) CL9_1",
+      "P-18B17CI474  CS49  (NFK) CL8_1",
+      "P-18B17BI473  BI41  (RJK) BPL",
+      "P-18B17CI474  CS46  (NTJ) CL9_2",
+      "P-18B17CI472  CS44 (RKI) CL1"
+    ]
+  },
+  {
+    "Day": "TUE",
+    "09:00 AM - 09:55 AM": [
+      "L-18B11CE415  CE41  (CPG) CR14",
+      "L-18B11CI415  BI41  (NFK) CR4",
+      "P-18B17CI473  CS49  (VKB) CL10",
+      "P-18B17CI474  CS410  (SWT) CL9_1",
+      "P-18B17CI471  CS41  (DGA) CL3_1",
+      "P-18B17CI471  CS44  (HRI) CL3_2",
+      "L-20B11EM412 EM41  (RKU) CR6",
+      "P-18B17CI472  CS47  (SGL) CL5_1",
+      "P-18B17CI472  CS42  (DHA) CL5_2",
+      "P-19B17CI471  IT41  (PKR) CL4",
+      "P-18B17CI472  CS45  (RKI) CL6",
+      "P-18B17CI473  CS48  (NVN) CL8_1"
+    ],
+    "10:00 AM - 10:55 AM": [
+      "L-18B11HS411  EC41,EM41,CE41,BT41,BI41,BACK-CE61 (ASA) LT3",
+      "P-18B17CI473  CS49  (VKB) CL10",
+      "P-18B17CI474  CS410  (SWT) CL9_1",
+      "P-18B17CI471  CS41  (DGA) CL3_1",
+      "P-18B17CI471  CS44  (HRI) CL3_2",
+      "P-18B17CI472  CS47  (SGL) CL5_1",
+      "P-18B17CI472  CS42  (DHA) CL5_2",
+      "P-19B17CI471  IT41  (PKR) CL4",
+      "P-18B17CI472  CS45  (RKI) CL6",
+      "P-18B17CI473  CS48  (NVN) CL8_1"
+    ],
+    "11:00 AM - 11:55 AM": [
+      "L-18B11HS411  CS41,CS42,CS43   (ASA) LT3",
+      "L-18B11CI414 CS410,IT41,IT42  (RKB) CR8",
+      "L-18B11CI414  CS44,CS45,CS46  (BKP) CR1",
+      "L-18B11CE412  CE41  (ASR) CR14",
+      "L-18B11BI412  BI41  (JAS) TR3",
+      "L-18B11BT411  BT41  (HSD) TR6",
+      "L-18B11GE411  CS47,CS48,CS49  (PNS) CR11"
+    ],
+    "12:00 PM - 12:55PM": [
+      "L-18B11BI413  BI41  (RJK) CR4",
+      "L-18B11BT411  BT41  (UDB) CR5"
+    ],
+    "01:00 PM - 01:55 PM": [
+      "L-18B11CI411  CS44,CS45,CS46  (HRI) CR1",
+      "L-18B11CI411  CS41,CS42,CS43   (PKG) CR2",
+      "L-18B11CI412  CS410,EM41,CSM  (AMN) CR4",
+      "L-18B11CI413  CS47,CS48,CS49  (SLD) CR5",
+      "P-18B17CI473  IT41  (VKB) CL9_1",
+      "P-19B17CI471  IT42  (PKR) CL1"
+    ],
+    "02:00 PM - 02:55 PM": [
+      "L-18B11CI414  CS47,CS48,CS49  (BKP) CR10",
+      "L-18B11CI412  CS41,CS42,CS43   (DHA) CR11",
+      "P-18B17CI473  IT41  (VKB) CL9_1",
+      "P-19B17CI471  IT42  (PKR) CL1",
+      "P-18B17BT472  BT41  (JTV) UG2",
+      "P-18B11CE413  CE41  (TNM) CADLAB",
+      "P-18B11MA412  BI41  (SST) BPL",
+      "L-18B11CI413  CS44,CS45,CS46  (NFK) CR9"
+    ],
+    "03:00 PM - 03:55 PM": [
+      "L-18B11HS411  CS410,IT41-IT42  (TGM) CR8",
+      "L-18B11CI412  CS47,CS48,CS49  (SGL) CR10",
+      "L-20B11EM411 EM41  (SWT) TR5",
+      "P-18B17CI471  CS46  (HRI) CL9_1",
+      "P-18B17CI474  CS45  (NTJ) CL9_2",
+      "P-18B17BT472  BT41  (JTV) UG2",
+      "P-18B11CE413  CE41  (TNM) CADLAB",
+      "P-18B11MA412  BI41  (SST) BPL",
+      "P-18B17CI472  CS41  (DHA) CL3_1",
+      "P-18B17CI472  CS44  (RKI) CL5_1"
+    ],
+    "04:00PM - 04:55 PM": [
+      "L-18B11HS411  CS47,CS48,CS49  (TGM) LT1",
+      "L-18B11GE411 IT41,IT42,BT41,BI41 (PNS) CR6",
+      "L-18B11GE411  CS410,EC41,EM41,ECM,CE41 (RRA) CR12",
+      "P-18B17CI471  CS46  (HRI) CL9_1",
+      "P-18B17CI474  CS45  (NTJ) CL9_2",
+      "P-18B17CI472  CS41  (DHA) CL3_1",
+      "P-18B17CI472  CS44  (RKI) CL5_1"
+    ]
+  },
+  {
+    "Day": "WED",
+    "09:00 AM - 09:55 AM": [
+      "L-18B11CE415  CE41  (CPG) CR14",
+      "P-18B17CI471  CS42  (DGA) CL3_2",
+      "P-18B17CI471  CS49  (MBI) CL4",
+      "P-18B17CI472  CS41  (DHA) CL5_1",
+      "P-18B17CI471  CS45  (HRI) CL5_2",
+      "P-18B17CI472  CS46  (AMN) CL6",
+      "P-18B17CI473  CS44  (PDG) CL7",
+      "L-20B11EM411 EM41  (SWT) TR4",
+      "L-18B11CI413  CS410,IT41,IT42  (SLD) CR8",
+      "P-18B17CI471  CS43  (SGL) CL9_1",
+      "L-18B11MA411  BI41  (SST) TR3",
+      "L-18B11EC411  EC41  (SHR) TR7",
+      "P-18B17CI474  CS47  (NVN) CL9_2"
+    ],
+    "10:00 AM - 10:55 AM": [
+      "L-18B11CE414  CE41  (NJP) CR14",
+      "L-18B11CI415  BI41  (NFK) CR11",
+      "P-18B17CI471  CS42  (DGA) CL3_2",
+      "P-18B17CI471  CS49  (MBI) CL4",
+      "P-18B17CI472  CS41  (DHA) CL5_1",
+      "P-18B17CI471  CS45  (HRI) CL5_2",
+      "P-18B17CI472  CS46  (AMN) CL6",
+      "P-18B17CI473  CS44  (PDG) CL7",
+      "L-18B11CI411  CS410,IT41,IT42 (PTK) CR8",
+      "P-18B17CI471  CS43  (SGL) CL9_1",
+      "P-20B17EM472 EM41  (VKB) CL8",
+      "L-18B11BT413  BT41  (RJK) TR4",
+      "P-18B17EC471  EC41  (SHR) ECL1",
+      "P-18B17CI474  CS47  (NVN) CL9_2"
+    ],
+    "11:00 AM - 11:55 AM": [
+      "L-18B11HS411  CS41,CS42,CS43   (ASA) LT3",
+      "L-18B11GE411  CS44,CS45,CS46  (PNS) CR1",
+      "L-18B11CI411  CS47,CS48,CS49  (MBI) CR6",
+      "L-18B11BI412  BI41  (JAS) TR7",
+      "L-18B11BT412  BT41  (JTV) TR5",
+      "L-18B11CI414  CS410.IT41,IT42  (RKB) CR5",
+      "P-20B17EM472 EM41  (VKB) CL8",
+      "P-18B17EC471  EC41  (SHR) ECL1"
+    ],
+    "12:00 PM - 12:55PM": [
+      "L-18B11HS411  EC41,EM41,CE41,BT41,BI41,BACK-CE61 (ASA) LT3"
+    ],
+    "01:00 PM - 01:55 PM": [
+      "L-18B11HS411 CS44,CS45,CS46, CS65-BACK (TGM) LT3",
+      "L-18B11CI413  CS41,CS42,CS43   (NFK) CR2",
+      "L-18B11CI414  CS47,CS48,CS49  (BKP) LT2",
+      "P-18B17CI473  CS410  (VKB) CL1"
+    ],
+    "02:00 PM - 02:55 PM": [
+      "L-18B11CI412  CS41,CS42,CS43   (DHA) CR1",
+      "L-19B11CI411  IT41-IT42  (PKR) CR11",
+      "L-18B11CI412  CS44,CS45,CS46  (RKI) CR10",
+      "L-18B11GE411  CS47,CS48,CS49  (PNS) LT2",
+      "P-18B17BT471  BT41  (HSD,UDB) BIOCHEMLAB",
+      "P-18B17CE471  CE41  (SUB) GEOLAB",
+      "P-18B17CI473  CS410  (VKB) CL1",
+      "P-18B17BI472  BI41  (AKN) GENOMELAB"
+    ],
+    "03:00 PM - 03:55 PM": [
+      "L-18B11CI411  CS44,CS45,CS46  (HRI) CR1",
+      "L-18B11CI411  CS41,CS42,CS43   (PKG) CR10",
+      "L-18B11CI412  CS47,CS48,CS49  (SGL) LT2",
+      "P-18B17BT471  BT41  (HSD,UDB) BIOCHEMLAB",
+      "P-18B17CE471  CE41  (SUB) GEOLAB",
+      "P-18B17CI471  IT42  (PTK) CL4",
+      "P-18B17BI472  BI41  (AKN) GENOMELAB",
+      "P-18B17CI472  CS410,CSM,EM41 (AMN) CL1"
+    ],
+    "04:00PM - 04:55 PM": [
+      "L-18B11CI414  CS44,CS45,CS46  (BKP) CR9",
+      "L-18B11HS411  CS47,CS48,CS49  (TGM) LT1",
+      "P-18B17CI471  IT42  (PTK) CL4",
+      "P-18B17CI472  CS410,CSM,EM41 (AMN) CL1"
+    ]
+  },
+  {
+    "Day": "THU",
+    "09:00 AM - 09:55 AM": [
+      "L-18B11BI414  BI41,BIM  (SML) CR7",
+      "P-18B17CI474  IT41  (SWT) CL9_2",
+      "P-18B17CI471  CS41  (DGA) CL7",
+      "P-18B17CI471  CS44  (HRI) CL3_2",
+      "P-18B17CI471  CS47  (MBI) CL4",
+      "P-18B17CI472  CS43  (AKJ) CL5_1",
+      "P-18B17CI472  CS48  (SGL) CL5_2",
+      "P-18B17EC371 EC41 (EMP) CL8_1",
+      "P-18B17CI473  CS46  (ALK) CL9_1",
+      "P-18B17CI472  CS49  (AMN) CL3_1",
+      "P-18B17CI471  CS410  (PTK) CL8_2",
+      "L-18B11BT412  BT41  (JTV) CR6"
+    ],
+    "10:00 AM - 10:55 AM": [
+      "L-18B11CE415  CE41  (CPG) CR14",
+      "P-18B17CI474  IT41  (SWT) CL9_2",
+      "P-18B17CI471  CS41  (DGA) CL7",
+      "P-18B17CI471  CS44  (HRI) CL3_2",
+      "P-18B17CI471  CS47  (MBI) CL4",
+      "P-18B17CI472  CS43  (AKJ) CL5_1",
+      "P-18B17CI472  CS48  (SGL) CL5_2",
+      "P-18B17EC371 EC41 (EMP) CL8_1",
+      "P-18B17CI473  CS46  (ALK) CL9_1",
+      "P-18B17CI472  CS49  (AMN) CL3_1",
+      "P-18B17CI471  CS410  (PTK) CL8_2",
+      "T-18B11CI415  BI41  (NFK) TR3",
+      "L-18B11BT413  BT41  (RJK) TR4",
+      "L-20B11EM412 EM41  (RKU) CR6"
+    ],
+    "11:00 AM - 11:55 AM": [
+      "L-18B11CI413  CS47,CS48,CS49  (SLD) LT3",
+      "L-18B11HS411  CS410,IT41,IT42  (TGM) CR8",
+      "L-18B11GE411  CS44,CS45,CS46  (PNS) CR9",
+      "L-18B11MA413  EC41,EM41  (PKP) TR6",
+      "L-18B11CE412  CE41  (ASR) CR14",
+      "L-18B11BI412  BI41  (JAS) CR7",
+      "L-18B11BT411  BT41  (HSD) TR7",
+      "L-18B11CI413  CS41,CS42,CS43   (NFK) CR10"
+    ],
+    "12:00 PM - 12:55PM": [
+      "L-18B11EC413  EC41,ECM [CS42,CS45,CS47,CS48] (ALK) TR1",
+      "L-18B11BT414  BT41  (AKN) CR7",
+      "L-18B11CE411  CE41  (AKG) TR9"
+    ],
+    "01:00 PM - 01:55 PM": [
+      "L-18B11CI411  CS44,CS45,CS46  (HRI) CR1",
+      "L-18B11CI411  CS41,CS42,CS43   (PKG) CR2",
+      "L-18B11CI412  CS410,EM41,CSM  (AMN) CR4",
+      "P-18B17CI473  IT41  (VKB) CL9_1",
+      "L-18B11EC413  EC41,ECM [CS42,CS45,CS47,CS48] (ALK) TR1",
+      "P-19B17CI471  IT42  (PKR) CL1"
+    ],
+    "02:00 PM - 02:55 PM": [
+      "L-18B11CI413  CS44,CS45,CS46  (NFK) CR1",
+      "L-18B11CI412  CS41,CS42,CS43   (DHA) CR9",
+      "L-18B11CI412  CS47,CS48,CS49  (SGL) CR10",
+      "P-18B17CI473  IT41  (VKB) CL9_1",
+      "P-18B17BT473  BT41  (TTS) BIL",
+      "P-19B17CI471  IT42  (PKR) CL1",
+      "L-18B11BI414  BI41,BIM  (SML) TR5"
+    ],
+    "03:00 PM - 03:55 PM": [
+      "L-18B11CI414  CS47,CS48,CS49  (BKP) LT2",
+      "L-18B11CI414  CS410,IT41-IT42  (RKB) CR11",
+      "T-20B11EM412 EM41  (RKU) CR6",
+      "P-18B17CI472  CS46  (AMN) CL8_1",
+      "L-18B11MA411  BI41  (SST) TR2",
+      "P-18B17BT473  BT41  (TTS) BIL",
+      "P-18B17CI474  CS43  (SLD) CL5_1",
+      "P-18B17CI472  CS42  (DHA) CL5_2",
+      "P-18B17CI472  CS45  (RKI) CL8",
+      "P-18B17CI474  CS44  (NFK) CL9_2"
+    ],
+    "04:00PM - 04:55 PM": [
+      "L-18B11HS411  CS47,CS48,CS49  (TGM) LT1",
+      "L-18B11GE411 IT41,IT42,BT41,BI41 (PNS) CR6",
+      "L-18B11GE411  CS410,EC41,EM41,ECM,CE41 (RRA) CR12",
+      "P-18B17CI472  CS46  (AMN) CL8_1",
+      "P-18B17CI474  CS43  (SLD) CL5_1",
+      "P-18B17CI472  CS42  (DHA) CL5_2",
+      "P-18B17CI472  CS45  (RKI) CL8",
+      "P-18B17CI474  CS44  (NFK) CL9_2"
+    ]
+  },
+  {
+    "Day": "FRI",
+    "09:00 AM - 09:55 AM": [
+      "L-18B11CE414  CE41  (NJP) CR13",
+      "P-18B17CI471  IT41  (PTK) CL3_1",
+      "P-18B17CI473  IT42  (VKB) CL7",
+      "P-18B17CI471  CS46  (HRI) CL3_2",
+      "P-18B17CI471  CS49  (MBI) CL5_2",
+      "P-18B17CI472  CS47  (SGL) CL6",
+      "L-18B11BI413  BI41  (RJK) TR4",
+      "P-18B17CI473 CS42 (PDG) CL8",
+      "P-18B17CI473  CS43  (ALK) CL9_1",
+      "P-18B17CI474  CS48  (NTJ) CL9_2",
+      "L-18B11BT411  BT41  (UDB) CR7",
+      "L-18B11EC411  EC41  (SHR) TR7"
+    ],
+    "10:00 AM - 10:55 AM": [
+      "L-18B11CE411  CE41  (AKG) CR14",
+      "P-18B17CI471  IT41  (PTK) CL3_1",
+      "P-18B17CI473  IT42  (VKB) CL7",
+      "P-18B17CI471  CS46  (HRI) Cl3_2",
+      "P-18B11CI474 BI41 (NFK) CL4",
+      "P-18B17CI471  CS49  (MBI) CL5_2",
+      "P-18B17CI472  CS47  (SGL) CL6",
+      "P-18B17CI473 CS42 (PDG) CL8",
+      "P-18B17CI473  CS43  (ALK) CL9_1",
+      "P-18B17CI474  CS48  (NTJ) CL9_2",
+      "L-18B11CI412  CS410,EM41,CSM  (AMN) CR7",
+      "T-18B11BT414  BT41  (AKN) TR5"
+    ],
+    "11:00 AM - 11:55 AM": [
+      "L-18B11CI411  CS410,IT41,IT42 (PTK) CR6",
+      "L-18B11HS411  CS41,CS42,CS43   (ASA) LT3",
+      "L-18B11CI411  CS47,CS48,CS49  (MBI) CR7",
+      "L-18B11CI414  CS44,CS45,CS46  (BKP) CR9",
+      "P-18B11CI474 BI41 (NFK) CL4",
+      "P-18B17CE472  CE41  (ASR) SURVLAB",
+      "P-18B17EC474  EC41,EM41   (NVN) CL1"
+    ],
+    "12:00 PM - 12:55PM": [
+      "P-18B17CE472  CE41  (ASR) SURVLAB",
+      "P-18B17EC474  EC41,EM41   (NVN) CL1"
+    ],
+    "01:00 PM - 01:55 PM": [
+      "L-18B11HS411 CS44,CS45,CS46, CS65-BACK (TGM) LT3",
+      "P-18B17CI471  CS48  (MBI) CL3_2",
+      "P-18B17CI473  CS49  (VKB) CL5_2",
+      "P-18B17CI471  CS410  (PTK) CL9_1",
+      "P-18B17CI472  CS43  (AKJ) CL9_2",
+      "P-18B17CI473  CS41  (ALK) CL7"
+    ],
+    "02:00 PM - 02:55 PM": [
+      "L-18B11CI412  CS44,CS45,CS46  (RKI) CR10",
+      "L-19B11CI411  IT41-IT42  (PKR) CR11",
+      "P-18B17CI471  CS48  (MBI) CL3_2",
+      "P-18B17CI473  CS49  (VKB) CL5_2",
+      "P-18B17CI471  CS410  (PTK) CL9_1",
+      "L-18B11BT414  BT41  (AKN) TR6",
+      "P-18B17CI472  CS43  (AKJ) CL9_2",
+      "P-18B17CI473  CS41  (ALK) CL7"
+    ],
+    "03:00 PM - 03:55 PM": [
+      "P-18B17CI472  CS410,CSM,EM41 (AMN) CL1",
+      "P-18B17CI473 CS45 (PDG) CL9_2",
+      "P-18B17CI474  IT42  (SWT) CL11",
+      "P-18B17BT474  BT41  (SDS) UG1",
+      "P-18B17CI473  CS47  (NVN) CL8_2",
+      "P-18B17CI471  IT41  (PTK) CL3_2",
+      "L-18B11GE411  CS41,CS42,CS43   (PNS) CR4"
+    ],
+    "04:00PM - 04:55 PM": [
+      "P-18B17CI472  CS410,CSM,EM41 (AMN) CL1",
+      "P-18B17CI473 CS45 (PDG) CL9_2",
+      "P-18B17CI474  IT42  (SWT) CL11",
+      "L-18B11EC311 EC41 (SRU) TR6",
+      "P-18B17BT474  BT41  (SDS) UG1",
+      "P-18B17CI473  CS47  (NVN) CL8_2",
+      "P-18B17CI471  IT41  (PTK) CL3_2",
+      "L-18B11CI414  CS41,CS42,CS43   (PKP) LT1"
+    ]
+  },
+  {
+    "Day": "SAT",
+    "09:00 AM - 09:55 AM": [
+      "P-18B17CI471  IT42  (PTK) CL9_1",
+      "P-18B17CI471  CS43  (SGL) CL4",
+      "P-18B17CI473  CS41  (ALK) CL7",
+      "P-18B17CI474  CS42  (SLD) CL10",
+      "P-19B17CI471  IT41  (PKR) CL6",
+      "P-18B17BI474  BI41,BIM  (SML) BIL"
+    ],
+    "10:00 AM - 10:55 AM": [
+      "P-18B17CI471  IT42  (PTK) Cl9_1",
+      "P-18B17CI471  CS43  (SGL) CL4",
+      "P-18B17CI473  CS41  (ALK) CL7",
+      "P-18B17CI474  CS42  (SLD) CL10",
+      "P-19B17CI471  IT41  (PKR) CL6",
+      "P-18B17BI474  BI41,BIM  (SML) BIL",
+      "L-18B11EC311 EC41 (SRU) TR6",
+      "L-20B11EM412 EM41  (RKU) CR7"
+    ],
+    "11:00 AM - 11:55 AM": [
+      "L-18B11CI413  CS47,CS48,CS49  (SLD) CR10",
+      "L-18B11GE411  CS41,CS42,CS43   (PNS) CR1",
+      "L-18B11MA413  EC41,EM41  (PKP) CR6"
+    ],
+    "12:00 PM - 12:55PM": [
+      "L-18B11CI414  CS41,CS42,CS43   (PKP) LT1",
+      "L-18B11EC413  EC41,ECM [CS42,CS45,CS47,CS48] (ALK) TR5",
+      "L-18B11CI415  BI41  (NFK) CR5",
+      "L-18B11CI413  CS410,IT41,IT42  (SLD) CR7"
+    ],
+    "01:00 PM - 01:55 PM": [
+      ""
+    ],
+    "02:00 PM - 02:55 PM": [
+      ""
+    ],
+    "03:00 PM - 03:55 PM": [
+      ""
+    ],
+    "04:00PM - 04:55 PM": [
+      ""
+    ]
+  }]
+    
+    //to get the dropdown options selected
+    var tableContainer = document.querySelector('#tableContainer');
+    
+    function getOption() {
+        var faculty = document.querySelector('#faculty');
+        var o1 = faculty.value.toLowerCase();
+        var batch = document.querySelector('#batch');
+        var o2 = batch.value.toLowerCase();
+        var venue = document.querySelector('#venue');
+        var o3 = venue.value.toLowerCase();
+        console.log(o1);
+
+        let table = document.createElement('table');
+        table.classList.add('myTable');
+
+        let headerRow = table.insertRow();
+        for (let key in data[0]) {
+          let headerCell = document.createElement('th');
+          headerCell.innerText = key;
+          headerCell.classList.add('headerCell');
+          headerRow.appendChild(headerCell);
+        }
+
+        for (let object of data) {
+          let hasMatch = false;
+          for (let key in object) {
+            let value = object[key];
+            if (
+              typeof value === 'string' &&
+              value.toLowerCase().includes(o1) &&
+              typeof value === 'string' &&
+              value.toLowerCase().includes(o2) &&
+              typeof value === 'string' &&
+              value.toLowerCase().includes(o3)
+            ) {
+              hasMatch = true;
+              break;
+            }
+          }
+          if (hasMatch) {
+            let row = table.insertRow();
+            for (let key in object) {
+              let cell = row.insertCell();
+              cell.innerText = object[key];
+              cell.classList.add('dataCell');
+            }
+          }
+        }
+
+        var tableContainer = document.querySelector('#tableContainer');
+        tableContainer.innerHTML = '';
+        tableContainer.appendChild(table);
+      }
+
+
+     export default getOption;
